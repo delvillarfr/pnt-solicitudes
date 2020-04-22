f239ff72033306f964870874af0d928848e69311
delvillarfr <delvillarfr@github.com>
2020-04-21 19:09:17 -0500

    Version used to finish scraping the directorios.

--- scrape.py
+++ scrape.py
@@ -167,153 +167,138 @@ def download_pnt_target_data(
 
     driver = webdriver.Firefox(firefox_profile=profile, options = opts)
     url = "https://consultapublicamx.inai.org.mx/vut-web/faces/view/consultaPublica.xhtml#inicio"
-    driver.get(url)
-
-    print("Driver initialized...")
-
-    # Open the menu options for the government level
-    options = get_web_elements(
-        driver, "//form[@id='formEntidadFederativa']//button", 1
-        )
-    options[0].click()
-
-    # Click on the desired government level
-    options = get_web_elements(
-        driver, "//form[@id='formEntidadFederativa']//a"
-        )
-    governments = [i.text for i in options]
-    options[governments.index(government)].click()
-
-    print("Government selected...")
-
-    # Click on a letter before selecting an institution
-    options = get_web_elements(
-        driver,
-        "//div[@id='formListaSujetosAZ:listaAZSujetos']/ol/child::li"
-        )
-    available_letters = [i.text for i in options]
-
-    letter = institution[0].upper()
-    letter_replaces = {
-            "Á": "A",
-            "É": "E",
-            "Í": "I",
-            "Ó": "O",
-            "Ú": "U"
-            }
-    for l in letter_replaces.keys():
-        letter = letter.replace(l, letter_replaces[l])
-
-    letter_index = available_letters.index(letter)
-    options[letter_index].click()
-
-
-    # Now select an institution. It must start with the stated letter
-    ## list entries are 1-indexed in the HTML
-    options = get_web_elements(
-        driver,
-        "//div[@id='formListaSujetosAZ:listaAZSujetos']/ol/li["
-        + str(letter_index + 1) + "]/div/ol/child::li/input"
-        )
-    institutions = [i.get_attribute("value") for i in options]
-    options[institutions.index(institution)].click()
-
-    print("Government institution selected...")
-
-
-    # Click on the dropdown menu to select the years.
-    button_years = get_web_elements(
-        driver, "//div[@id='periodoOriginal']/div/button", 1
-        )
 
     try:
-        button_years[0].click()
-    # This happens when the institution has no obligations
-    except ElementClickInterceptedException:
-        driver.quit()
-        return None
+        driver.get(url)
 
-    # Select a year from the dropdown menu
-    year_options = get_web_elements(
-        driver, "//div[@id='periodoOriginal']//a"
-        )
-    years = [i.text for i in year_options]
+        print("Driver initialized...")
 
-    # Click again on the menu to enter loop...
-    button_years[0].click()
+        # Open the menu options for the government level
+        options = get_web_elements(
+            driver, "//form[@id='formEntidadFederativa']//button", 1
+            )
+        options[0].click()
+
+        # Click on the desired government level
+        options = get_web_elements(
+            driver, "//form[@id='formEntidadFederativa']//a"
+            )
+        governments = [i.text for i in options]
+        options[governments.index(government)].click()
+
+        print("Government selected...")
+
+        # Click on a letter before selecting an institution
+        options = get_web_elements(
+            driver,
+            "//div[@id='formListaSujetosAZ:listaAZSujetos']/ol/child::li"
+            )
+        available_letters = [i.text for i in options]
+
+        letter = institution[0].upper()
+        letter_replaces = {
+                "Á": "A",
+                "É": "E",
+                "Í": "I",
+                "Ó": "O",
+                "Ú": "U"
+                }
+        for l in letter_replaces.keys():
+            letter = letter.replace(l, letter_replaces[l])
+
+        letter_index = available_letters.index(letter)
+        options[letter_index].click()
 
 
-    for year in years:
+        # Now select an institution. It must start with the stated letter
+        ## list entries are 1-indexed in the HTML
+        options = get_web_elements(
+            driver,
+            "//div[@id='formListaSujetosAZ:listaAZSujetos']/ol/li["
+            + str(letter_index + 1) + "]/div/ol/child::li/input"
+            )
+        institutions = [i.get_attribute("value") for i in options]
+        options[institutions.index(institution)].click()
+
+        print("Government institution selected...")
+
+
         # Click on the dropdown menu to select the years.
         button_years = get_web_elements(
             driver, "//div[@id='periodoOriginal']/div/button", 1
             )
-        button_years[0].click()
+
+        try:
+            button_years[0].click()
+        # This happens when the institution has no obligations
+        except ElementClickInterceptedException:
+            driver.quit()
+            return None
 
         # Select a year from the dropdown menu
         year_options = get_web_elements(
             driver, "//div[@id='periodoOriginal']//a"
             )
-        year_options[years.index(year)].click()
+        years = [i.text for i in year_options]
 
-        print("Year range " + str(year) + " selected...")
+        # Click again on the menu to enter loop...
+        button_years[0].click()
 
-        time.sleep(2)
 
-        # Now select the type of data to download, e.g. the Directorio
-        ## If there are no obligations, get outta here.
-        try:
-            options = get_web_elements(
-                driver,
-                "//div[@id='formListaObligaciones:listaObligacionesTransparencia']/div[1]/child::label",
-                secs_timeout = 5
+        for year in years:
+            # Click on the dropdown menu to select the years.
+            button_years = get_web_elements(
+                driver, "//div[@id='periodoOriginal']/div/button", 1
                 )
-        except TimeoutException:
-            break
+            button_years[0].click()
 
-        obligations = [i.text for i in options]
-        obligation_ids = [i.get_attribute("id") for i in options]
-
-        # Proceed only if the obligation exists...
-        if obligation in obligations:
-            options[obligations.index(obligation)].click()
-
-
-            # Click download
-            options = get_web_elements(
-                driver,
-                "//a[starts-with(@id,'formDescargaArchivos')]"
+            # Select a year from the dropdown menu
+            year_options = get_web_elements(
+                driver, "//div[@id='periodoOriginal']//a"
                 )
-            options_labels = [i.text for i in options]
+            year_options[years.index(year)].click()
 
-            # Skip if there is no download available
-            if "DESCARGAR" in options_labels:
-                options = options[options_labels.index("DESCARGAR")].click()
+            print("Year range " + str(year) + " selected...")
 
+            time.sleep(3)
 
-                # Select Excel download
+            # Now select the type of data to download, e.g. the Directorio
+            ## If there are no obligations, get outta here.
+            try:
                 options = get_web_elements(
-                    driver, "//span[@id='descargaExcel']//label", 1
+                    driver,
+                    "//div[@id='formListaObligaciones:listaObligacionesTransparencia']/div[1]/child::label",
+                    secs_timeout = 5
                     )
-                options[0].click()
+            except TimeoutException:
+                break
+
+            obligations = [i.text for i in options]
+            obligation_ids = [i.get_attribute("id") for i in options]
+
+            # Proceed only if the obligation exists...
+            if obligation in obligations:
+                options[obligations.index(obligation)].click()
 
 
-                # Click on range
-                button_excel_range = get_web_elements(
-                    driver, "//span[@id='formModalRangos:panelRangoExcel']//button", 1
+                # Click download
+                options = get_web_elements(
+                    driver,
+                    "//a[starts-with(@id,'formDescargaArchivos')]"
                     )
-                button_excel_range[0].click()
+                options_labels = [i.text for i in options]
 
-                # Select range
-                options = get_web_elements(driver, "//form[@id='formModalRangos']//a")
-                excel_ranges = [i.text for i in options]
+                # Skip if there is no download available
+                if "DESCARGAR" in options_labels:
+                    options = options[options_labels.index("DESCARGAR")].click()
 
 
-                # Click the excel range button again to enter loop...
-                button_excel_range[0].click()
+                    # Select Excel download
+                    options = get_web_elements(
+                        driver, "//span[@id='descargaExcel']//label", 1
+                        )
+                    options[0].click()
 
-                # First entry in the range is "Selecciona"
-                for excel_range in excel_ranges[1: ]:
 
                     # Click on range
                     button_excel_range = get_web_elements(
@@ -322,43 +307,60 @@ def download_pnt_target_data(
                     button_excel_range[0].click()
 
                     # Select range
-                    options = get_web_elements(driver, "//form[@id='formModalRangos']//a") 
-                    options[excel_ranges.index(excel_range)].click()
+                    options = get_web_elements(driver, "//form[@id='formModalRangos']//a")
+                    excel_ranges = [i.text for i in options]
 
-                    print("Download initialized for range " + str(excel_range) + "...")
 
-                    # Count the number of files in the target directory
-                    files_pre = os.listdir(target_dir)
+                    # Click the excel range button again to enter loop...
+                    button_excel_range[0].click()
 
-                    ## hit Download
-                    options = get_web_elements(
-                        driver,
-                        "//input[@id='formModalRangos:btnDescargaExcel']"
-                        )
-                    options[0].click()
+                    # First entry in the range is "Selecciona"
+                    for excel_range in excel_ranges[1: ]:
 
-                    ## Check if file has downloaded
-                    files_post = files_pre
-                    while (
-                            (len(files_pre) + 1) != len(files_post)
-                            or any([f[-3: ] != "xls" for f in files_post])
-                        ):
-                        time.sleep(5)
-                        files_post = os.listdir(target_dir)
+                        # Click on range
+                        button_excel_range = get_web_elements(
+                            driver, "//span[@id='formModalRangos:panelRangoExcel']//button", 1
+                            )
+                        button_excel_range[0].click()
 
-                    print("Download finalized...")
+                        # Select range
+                        options = get_web_elements(driver, "//form[@id='formModalRangos']//a") 
+                        options[excel_ranges.index(excel_range)].click()
 
-                # Close the download dialog by hitting <esc>
-                webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+                        print("Download initialized for range " + str(excel_range) + "...")
+
+                        # Count the number of files in the target directory
+                        files_pre = os.listdir(target_dir)
+
+                        ## hit Download
+                        options = get_web_elements(
+                            driver,
+                            "//input[@id='formModalRangos:btnDescargaExcel']"
+                            )
+                        options[0].click()
+
+                        ## Check if file has downloaded
+                        files_post = files_pre
+                        while (
+                                (len(files_pre) + 1) != len(files_post)
+                                or any([f[-3: ] != "xls" for f in files_post])
+                            ):
+                            time.sleep(2)
+                            files_post = os.listdir(target_dir)
+
+                        print("Download finalized...")
+
+                    # Close the download dialog by hitting <esc>
+                    webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+                    time.sleep(2)
+
+            else:
                 time.sleep(2)
 
-        else:
-            time.sleep(2)
 
-
-    ## Close session
-
-    driver.quit()
+    finally:
+        ## ALWAYS close the session
+        driver.quit()
 
     return None
 
